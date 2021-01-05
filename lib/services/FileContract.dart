164ad8001d05ee6b905b367d7cc3870bc0663813
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2021-01-05 16:46:56 +0000

    Update

--- lib/services/FileContract.dart
+++ lib/services/FileContract.dart
@@ -1,7 +1,11 @@
+import 'package:image_picker/image_picker.dart';
+
 abstract class FileContract {
   Future<String> getPath();
 
   Future<bool> writeFile(String data);
 
   Future<String> readFile();
+
+  Future getImage(ImageSource source);
 }
