1e0bf8512e7b37524b9a12f9bec6ad8574745826
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2021-02-18 14:07:34 +0000

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'MyNote',
-        darkTheme: ThemeData.dark().copyWith(
+        theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: Colors.black,
           primaryColor: Colors.white,
           buttonColor: Color.fromRGBO(112, 237, 238, 5),
@@ -33,17 +33,17 @@ class MyApp extends StatelessWidget {
           disabledColor: Color.fromRGBO(182, 182, 182, 50),
           brightness: Brightness.light,
         ),
-        theme: ThemeData(
-          brightness: Brightness.light,
-          floatingActionButtonTheme: FloatingActionButtonThemeData(
-            backgroundColor: Colors.purple,
-          ),
-          scaffoldBackgroundColor: Colors.white,
-          primaryColor: Colors.black,
-          unselectedWidgetColor: Color.fromRGBO(235, 235, 235, 6),
-          buttonColor: Colors.lightBlueAccent,
-          disabledColor: Colors.grey.shade300,
-        ),
+        // theme: ThemeData(
+        //   brightness: Brightness.light,
+        //   floatingActionButtonTheme: FloatingActionButtonThemeData(
+        //     backgroundColor: Colors.purple,
+        //   ),
+        //   scaffoldBackgroundColor: Colors.white,
+        //   primaryColor: Colors.black,
+        //   unselectedWidgetColor: Color.fromRGBO(235, 235, 235, 6),
+        //   buttonColor: Colors.lightBlueAccent,
+        //   disabledColor: Colors.grey.shade300,
+        // ),
         home: Splash(),
         routes: {
           '/home': (BuildContext context) => MyHomePage(),
