28dbfb8cf18da3acad9e9f9360fce166ec2c8482
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2021-02-18 07:46:04 +0000

    Updated lockscreen

--- lib/screens/LockScreen.dart
+++ lib/screens/LockScreen.dart
@@ -1,16 +1,147 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:my_note/models/Note.dart';
+import 'package:my_note/screens/Workspace.dart';
+import 'package:my_note/services/local_auth_service.dart';
 
 class LockScreen extends StatefulWidget {
+  final Note note;
+
+  const LockScreen({Key key, this.note}) : super(key: key);
   @override
   _LockScreenState createState() => _LockScreenState();
 }
 
 class _LockScreenState extends State<LockScreen> {
+  TextEditingController pinCode = TextEditingController();
+  LocalAuthService authService = LocalAuthService();
+
+  @override
+  void initState() {
+    // authService.runBoimetric().then((value) {
+    //   Navigator.push(context, MaterialPageRoute(builder: (context) {
+    //     return WorkSpace(existingNote: widget.note);
+    //   }));
+    // });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Theme.of(context).primaryColor,
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+        elevation: 0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        title: Text(
+          'Locked',
+          style: TextStyle(color: Theme.of(context).primaryColor),
+        ),
+      ),
       body: Center(
-        child: Text('LockScreen'),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Enter pin code to unlock note',
+                style: TextStyle(color: Colors.grey),
+              ),
+              SizedBox(height: 30),
+              TextFormField(
+                controller: pinCode,
+                keyboardType: TextInputType.number,
+                autofocus: true,
+                onChanged: (text) {},
+                maxLength: 4,
+                inputFormatters: [
+                  LengthLimitingTextInputFormatter(4),
+                ],
+                maxLines: 1,
+                maxLengthEnforced: true,
+                buildCounter: (context, {currentLength, maxLength, isFocused}) {
+                  return Container(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      currentLength.toString() + "/" + maxLength.toString(),
+                      style: TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                  );
+                },
+                decoration: InputDecoration(
+                  isDense: true,
+                  contentPadding: EdgeInsets.all(15),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(width: 1, color: Colors.black),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(width: 1, color: Colors.grey),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  hintText: 'Pin',
+                  hintStyle: TextStyle(
+                    fontSize: 17,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              SizedBox(height: 50),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Use biometrics',
+                  style: TextStyle(
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Tap on fingerprint to unlock',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.center,
+                child: Icon(
+                  Icons.fingerprint,
+                  size: 70,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return WorkSpace(existingNote: widget.note);
+          }));
+        },
+        child: Icon(Icons.arrow_forward),
       ),
     );
   }
