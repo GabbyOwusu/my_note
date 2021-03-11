7b02b49352b2af4d93b6cfc201a61df2c5b334ff
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2021-03-11 15:41:31 +0000

    Update

---
+++ lib/widgets/logo.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Logo extends StatelessWidget {
+  const Logo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text.rich(
+      TextSpan(
+        text: "My",
+        style: TextStyle(
+          fontSize: 26,
+          fontWeight: FontWeight.w900,
+          color: Theme.of(context).primaryColor,
+        ),
+        children: [
+          TextSpan(
+            text: "Note",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w400,
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+          TextSpan(
+            text: ".",
+            style: TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.bold,
+              color: Colors.purple,
+            ),
+          ),
+        ],
+      ),
+      style: TextStyle(
+        color: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+}
