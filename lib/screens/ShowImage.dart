21a0e551debb90c6cb93631ba1e4063e4d1bbd75
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2021-03-23 11:44:06 +0000

    Image Screen

---
+++ lib/screens/ShowImage.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:my_note/models/Note.dart';
+
+class ShowImage extends StatelessWidget {
+  final Note note;
+  final Function deleteImage;
+
+  const ShowImage({Key key, @required this.note, this.deleteImage})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [IconButton(icon: Icon(Icons.delete), onPressed: deleteImage)],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(60),
+        child: Hero(
+          tag: '${note.imagePath}',
+          child: Image.file(File(note.imagePath)),
+        ),
+      ),
+    );
+  }
+}
