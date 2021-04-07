6cfc529980c84bdebcffea6172b13eaa29792982
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2021-04-07 20:03:02 +0100

    Update

--- lib/widgets/bottom_controls.dart
+++ lib/widgets/bottom_controls.dart
@@ -1,120 +1,120 @@
-import 'dart:ui';
+// import 'dart:ui';
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-import 'package:my_note/models/Note.dart';
-import 'package:my_note/providers/notes_provider.dart';
-import 'package:my_note/screens/ocr_screen.dart';
-import 'package:my_note/widgets/reocrd_sheet.dart';
+// import 'package:my_note/models/Note.dart';
+// import 'package:my_note/providers/notes_provider.dart';
+// import 'package:my_note/screens/ocr_screen.dart';
+// import 'package:my_note/widgets/reocrd_sheet.dart';
 
-class BottomControls extends StatefulWidget {
-  final Note note;
-  final NotesProvider provider;
-  final Function reset;
+// class BottomControls extends StatefulWidget {
+//   final Note note;
+//   final NotesProvider provider;
+//   final Function reset;
 
-  const BottomControls({
-    Key key,
-    @required this.note,
-    @required this.provider,
-    @required this.reset,
-  }) : super(key: key);
-  @override
-  _BottomControlsState createState() => _BottomControlsState();
-}
+//   const BottomControls({
+//     Key key,
+//     @required this.note,
+//     @required this.provider,
+//     @required this.reset,
+//   }) : super(key: key);
+//   @override
+//   _BottomControlsState createState() => _BottomControlsState();
+// }
 
-class _BottomControlsState extends State<BottomControls> {
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Stack(
-        children: [
-          ClipRect(
-            child: BackdropFilter(
-              filter: ImageFilter.blur(),
-              child: Container(
-                alignment: Alignment.bottomCenter,
-                color: Colors.grey.shade100.withOpacity(0.5),
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(bottom: 30, left: 30, right: 30),
-            padding: const EdgeInsets.all(5),
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-              borderRadius: BorderRadius.circular(30),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 20),
-                  child: IconButton(
-                    icon: Icon(Icons.camera_enhance),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Ocr(),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 20),
-                  child: IconButton(
-                    icon: Icon(Icons.mic, size: 30),
-                    onPressed: () {
-                      showModalBottomSheet(
-                          context: context,
-                          builder: (context) {
-                            return Recording();
-                          });
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 20),
-                  child: IconButton(
-                    icon: Icon(Icons.image_outlined),
-                    onPressed: () async {
-                      widget.provider.addImage(widget.note);
-                    },
-                  ),
-                ),
-                if (widget.note.locked == false)
-                  Padding(
-                    padding: const EdgeInsets.only(right: 20),
-                    child: IconButton(
-                      onPressed: () async {
-                        setState(() {
-                          widget.provider.lockNote(widget.note);
-                        });
-                      },
-                      icon: Icon(
-                        Icons.lock_open,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  )
-                else
-                  Padding(
-                    padding: const EdgeInsets.only(right: 20),
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.lock,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      onPressed: widget.reset,
-                    ),
-                  ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+// class _BottomControlsState extends State<BottomControls> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Align(
+//       alignment: Alignment.bottomCenter,
+//       child: Stack(
+//         children: [
+//           ClipRect(
+//             child: BackdropFilter(
+//               filter: ImageFilter.blur(),
+//               child: Container(
+//                 alignment: Alignment.bottomCenter,
+//                 color: Colors.grey.shade100.withOpacity(0.5),
+//               ),
+//             ),
+//           ),
+//           Container(
+//             margin: EdgeInsets.only(bottom: 30, left: 30, right: 30),
+//             padding: const EdgeInsets.all(5),
+//             decoration: BoxDecoration(
+//               color: Colors.transparent,
+//               borderRadius: BorderRadius.circular(30),
+//             ),
+//             child: Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceAround,
+//               children: [
+//                 Padding(
+//                   padding: const EdgeInsets.only(right: 20),
+//                   child: IconButton(
+//                     icon: Icon(Icons.camera_enhance),
+//                     onPressed: () {
+//                       Navigator.push(
+//                         context,
+//                         MaterialPageRoute(
+//                           builder: (context) => Ocr(),
+//                         ),
+//                       );
+//                     },
+//                   ),
+//                 ),
+//                 Padding(
+//                   padding: const EdgeInsets.only(right: 20),
+//                   child: IconButton(
+//                     icon: Icon(Icons.mic, size: 30),
+//                     onPressed: () {
+//                       showModalBottomSheet(
+//                           context: context,
+//                           builder: (context) {
+//                             return Recording();
+//                           });
+//                     },
+//                   ),
+//                 ),
+//                 Padding(
+//                   padding: const EdgeInsets.only(right: 20),
+//                   child: IconButton(
+//                     icon: Icon(Icons.image_outlined),
+//                     onPressed: () async {
+//                       widget.provider.addImage(widget.note);
+//                     },
+//                   ),
+//                 ),
+//                 if (widget.note.locked == false)
+//                   Padding(
+//                     padding: const EdgeInsets.only(right: 20),
+//                     child: IconButton(
+//                       onPressed: () async {
+//                         setState(() {
+//                           widget.provider.lockNote(widget.note);
+//                         });
+//                       },
+//                       icon: Icon(
+//                         Icons.lock_open,
+//                         color: Colors.grey,
+//                       ),
+//                     ),
+//                   )
+//                 else
+//                   Padding(
+//                     padding: const EdgeInsets.only(right: 20),
+//                     child: IconButton(
+//                       icon: Icon(
+//                         Icons.lock,
+//                         color: Theme.of(context).primaryColor,
+//                       ),
+//                       onPressed: widget.reset,
+//                     ),
+//                   ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
