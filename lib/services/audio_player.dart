473f87cef923d3bd9673a3dd1ac1b7da74efe9ad
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2021-04-07 20:02:23 +0100

    Added audio player class

---
+++ lib/services/audio_player.dart
@@ -0,0 +1,19 @@
+import 'package:audioplayer/audioplayer.dart';
+
+class PlayAudio {
+  AudioPlayer player = AudioPlayer();
+
+  Future<void> playAudio(String path) async {
+    try {
+      await player.play(path);
+    } catch (e) {
+      print('The audio cannot be played because $e');
+    }
+  }
+
+  Future<void> pauseAudio() async {
+    await player.stop();
+  }
+
+  Future<void> seek() async {}
+}
