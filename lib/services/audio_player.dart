eecdaafd89ce11f531b4b0288d236fa1aeab28b4
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2021-05-08 19:51:50 +0100

    Update

--- lib/services/audio_player.dart
+++ lib/services/audio_player.dart
@@ -1,22 +1,12 @@
-import 'package:audioplayer/audioplayer.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class PlayAudio {
-  AudioPlayer player = AudioPlayer();
-
-  Future<void> playAudio(String path) async {
+  Future<void> playAudio(AudioPlayer player, String path) async {
     try {
       await player.play(path, isLocal: true);
     } catch (e) {
       print('The audio cannot be played because $e');
     }
-  }
-
-  Future<void> stopAudio() async {
-    await player.stop();
-  }
-
-  Future<void> pauseAudio() async {
-    await player.pause();
   }
 
   Future<void> seek() async {}
